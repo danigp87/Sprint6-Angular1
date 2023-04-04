d842336f757af84858ebfee900aed96516417a27
danigp87 <danielgarciaportillo@gmail.com>
2023-04-04 12:59:03 +0200

    Nivel2-Ejercicio5 Acabado

--- src/app/escena/escena/escena.component.ts
+++ src/app/escena/escena/escena.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Frases } from '../../home/home/home.component';
 
 @Component({
   selector: 'app-escena',
@@ -6,5 +7,33 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./escena.component.scss']
 })
 export class EscenaComponent {
-  @Input() frases: string[] = []
+  @Input() frases: Frases[] = []
+
+  prev() {
+    for (let i = 0; i < this.frases.length; i++) {
+      if (this.frases[i].fondo === true) {
+        this.frases[i].fondo = false;
+        if (this.frases[i].valor === 0) {
+          this.frases[3].fondo = true;
+        } else {
+          this.frases[i - 1].fondo = true;
+        }
+        break;
+      }
+    }
+  }
+
+  next() {
+    for (let i = 0; i < this.frases.length; i++) {
+      if (this.frases[i].fondo === true) {
+        this.frases[i].fondo = false;
+        if (this.frases[i].valor === 3) {
+          this.frases[0].fondo = true;
+        } else {
+          this.frases[i + 1].fondo = true;
+        }
+        break;
+      }
+    }
+  }
 }
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+export { Frases }
+
+interface Frases {
+  valor: number;
+  frase: string;
+  fondo?: boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent {
 
-  frase: string[] = [
-    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial.",
-    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-    "L'heroi va decidir travessar la porta que el portava a casa.",
-    "Mentrestant, altres herois no van tenir tanta sort en la seva elecció..."
-  ]
+  frase: Frases[] = [
+    { valor: 0, frase: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial.", fondo: true },
+    { valor: 1, frase: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", fondo: false },
+    { valor: 2, frase: "L'heroi va decidir travessar la porta que el portava a casa.", fondo: false },
+    { valor: 3, frase: "Mentrestant, altres herois no van tenir tanta sort en la seva elecció...", fondo: false }
+  ];
+
+  @Output() frases: Frases[] = []
+
+  mostrarFrases = true
+  mostrar() {
+    this.mostrarFrases = false
+  }
+
 }
+
